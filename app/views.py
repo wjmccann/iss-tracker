52ae4a5468e458314d6bec87e5c735482a4b6109
wjmccann <wjmccann@live.com.au>
2017-02-04 22:18:47 +1100

    add ajax marker

--- app/views.py
+++ app/views.py
@@ -1,17 +1,19 @@
 from app import app
-from flask import render_template
+from flask import render_template, jsonify
 import urllib.request
 import json
 
-@app.route('/')
-def index():
-
+@app.route('/getloc')
+def getloc():
 	req = urllib.request.Request("http://api.open-notify.org/iss-now.json")
 	response = urllib.request.urlopen(req)
 	obj = json.loads(response.read().decode('utf-8'))
 	lat = obj['iss_position']['latitude']
 	lng = obj['iss_position']['longitude']
-	
-	return render_template('index.html',
-							lat=lat,
-							lng=lng)
+
+	return jsonify(lat=lat, lng=lng)
+
+@app.route('/')
+def index():
+
+	return render_template('index.html')
